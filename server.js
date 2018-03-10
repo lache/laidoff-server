297b39c6f0da61a06721f860dbb8b093629ae74a
jaeyoung.choi <lactrious@gmail.com>
2018-03-10 15:44:20 +0900

    Define structs

---
+++ server.js
@@ -0,0 +1,23 @@
+const Struct = require('struct')
+
+const HttpResponseStruct = Struct()
+    .word16Sle('size')
+    .word16Sle('type')
+    .chars('body', 8192 - 4)
+
+const FullStateObjectStruct = Struct()
+    .word32Sle('id')
+    .floatle('x')
+    .floatle('y')
+    .floatle('a')
+
+const FullStateStruct = Struct()
+    .word8Sle('type')
+    .word8Sle('padding0')
+    .word8Sle('padding1')
+    .word8Sle('padding2')
+    .array('objects', 64, FullStateObjectStruct)
+
+FullStateStruct.allocate()
+const buf = FullStateStruct.buffer()
+console.log(buf)
