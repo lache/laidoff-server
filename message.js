444bf8f699f5c66800b50b38636dd7dc5019f402
lacti <lactrious@gmail.com>
2018-03-11 23:37:21 +0900

    Delete test files and add lint and prettier

--- message.js
+++ message.js
@@ -1,23 +1,26 @@
 const Struct = require('struct')
 // const dgram = require("dgram");
 
+/*
+Not yet used.
 const HttpResponseStruct = Struct()
-    .word16Sle('size')
-    .word16Sle('type')
-    .chars('body', 8192 - 4)
+  .word16Sle('size')
+  .word16Sle('type')
+  .chars('body', 8192 - 4)
+*/
 
 const FullStateObjectStruct = Struct()
-    .word32Sle('id')
-    .floatle('x')
-    .floatle('y')
-    .floatle('a')
+  .word32Sle('id')
+  .floatle('x')
+  .floatle('y')
+  .floatle('a')
 
 const FullStateStruct = Struct()
-    .word8Sle('type')
-    .word8Sle('padding0')
-    .word8Sle('padding1')
-    .word8Sle('padding2')
-    .array('objects', 64, FullStateObjectStruct)
+  .word8Sle('type')
+  .word8Sle('padding0')
+  .word8Sle('padding1')
+  .word8Sle('padding2')
+  .array('objects', 64, FullStateObjectStruct)
 
 FullStateStruct.allocate()
 const buf = FullStateStruct.buffer()
--- ttl.schema.sql
+++ ttl.schema.sql
@@ -1,28 +1,6 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
---
--- Host: localhost    Database: ttl
--- ------------------------------------------------------
--- Server version	5.7.21-0ubuntu0.16.04.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `mission`
---
-
-DROP TABLE IF EXISTS `mission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `mission` (
+-- DROP TABLE IF EXISTS `mission`;
+CREATE TABLE IF NOT EXISTS `mission` (
   `mission_id` int(11) NOT NULL AUTO_INCREMENT,
   `departure_id` int(11) NOT NULL,
   `arrival_id` int(11) NOT NULL,
@@ -30,33 +8,19 @@ CREATE TABLE `mission` (
   `expired` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`mission_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB CHARSET=utf8mb4;
 
---
--- Table structure for table `region`
---
-
-DROP TABLE IF EXISTS `region`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `region` (
+-- DROP TABLE IF EXISTS `region`;
+CREATE TABLE IF NOT EXISTS `region` (
   `region_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) NOT NULL,
   `x` float NOT NULL,
   `y` float NOT NULL,
   PRIMARY KEY (`region_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB CHARSET=utf8mb4;
 
---
--- Table structure for table `ship`
---
-
-DROP TABLE IF EXISTS `ship`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ship` (
+-- DROP TABLE IF EXISTS `ship`;
+CREATE TABLE IF NOT EXISTS `ship` (
   `ship_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `name` varchar(80) NOT NULL DEFAULT 'Bad Gateway',
@@ -66,17 +30,10 @@ CREATE TABLE `ship` (
   `oil` float NOT NULL DEFAULT '200000',
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ship_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB CHARSET=utf8mb4;
 
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+-- DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `guid` varchar(36) NOT NULL,
   `name` varchar(80) NOT NULL DEFAULT 'Lee',
@@ -84,16 +41,4 @@ CREATE TABLE `user` (
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `ux_guid` (`guid`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-03-11  1:05:15
+) ENGINE=InnoDB CHARSET=utf8mb4;
