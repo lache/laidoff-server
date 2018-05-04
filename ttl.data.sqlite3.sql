b7caf293a12bb195a1302f867ffd070447006b44
Geoyeob Kim <gasbank@msn.com>
2018-05-04 13:13:21 +0900

    do not populate region table on '--init'

--- ttl.data.sqlite3.sql
+++ ttl.data.sqlite3.sql
@@ -1,33 +1,5 @@
 -- regions
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('베르겐',60.23,5.2);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('오슬로',60.12,11.05);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('오슬로',59.57,10.43);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('스톡홀름',59.34,18.06);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('헬싱키',60.19,24.58);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('브뤼셀',50.48,4.21);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('제네바',46.15,6.08);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('베를린',52.28,13.24);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('프라하',50.01,14.27);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('바르샤바',52.1,20.58);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('부다페스트',47.26,19.11);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('베오그라드',44.48,20.28);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('자그레브',45.49,15.59);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('이스탄불',40.58,29.05);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('베르호얀스크',67.33,133.23);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('민스크',53.56,27.38);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('모스코바',55.5,37.37);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('키에프',50.24,30.34);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('타시켄트',41.16,69.16);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('울란바토르',47.55,106.52);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('평양',39.02,125.47);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('도꾜',35.41,139.46);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('오사까',34.41,135.31);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('베이징',39.56,116.17);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('라싸',29.4,91.08);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('상하이',31.24,121.28);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('튜니스',36.5,10.14);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('파라쿠',9.21,2.37);
-INSERT INTO `region` (`name`, `x`, `y`) VALUES ('쇼와',69,39.35);
+
 
 -- missions
 INSERT INTO `mission` (`departure_id`, `arrival_id`, `reward`, `expired`, `updated`) VALUES (1,2,1200,'2019-03-10 06:48:47','2018-03-10 06:48:47');
